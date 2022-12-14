ed9099641e7f3a63a28e5a92122dad5a41e851a3
NouranOmar98 <norankinane@gmail.com>
2022-12-14 21:31:16 +0300

    tasks

--- 12 Dec/SQLQuery4.sql
+++ 12 Dec/SQLQuery4.sql
@@ -1,25 +1,18 @@
+
 create table customers(
 id int primary key,
 firstname char (20) not null,
 lastname char (20) not null,
 job char (30),
 );
-
-
-insert into customers values(123,'Rogina', 'Irshaidat', 'Full-Stack');
-select * from customers;
  
- --//
+--//
 
 create table orders(
 id int primary key,
 price float,
 total float,
 );
-
-insert into orders values(589,258, 3);
-select * from orders;
-
 --//
 
 
@@ -30,11 +23,6 @@ price float not null,
 );
 
 --//
-
-insert into product values(123,'Razan', 586);
-select * from product;
-
---//
 create table employee(
 id int primary key,
 firstname char (20) not null,
@@ -43,7 +31,16 @@ job char (30),
 );
 
 insert into employee values(123,'Nouran', 'Omar', 'Full-Stack');
+insert into customers values(123,'Rogina', 'Irshaidat', 'Full-Stack');
+insert into orders values(589,258, 3);
+insert into product values(123,'Razan', 586);
+
+
+select * from customers;
+select * from orders;
+select * from product;
 select * from employee;
+
 
 
 
---
+++ 13 Dec/SQLQuery2.sql
@@ -0,0 +1,65 @@
+CREATE TABLE Customers(
+    CustomerID INT IDENTITY(1,1),
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	Job VARCHAR(200),
+	PRIMARY KEY(CustomerID)
+);
+
+--//
+CREATE TABLE  Orders(
+    OrderID INT IDENTITY(1,1),
+    CustomerID INT NOT NULL,
+    Price FlOAT,
+    Total FlOAT,
+	PRIMARY KEY(OrderID),
+	FOREIGN KEY (CustomerID) REFERENCES Customers
+);
+
+--//
+CREATE TABLE Product(
+    ProductID INT IDENTITY(1,1),
+	OrderID INT,
+    Name CHAR (20) not null,
+    Price FLOAT not null,
+	PRIMARY KEY (ProductID),
+	FOREIGN KEY (OrderID) REFERENCES Orders
+);
+
+Insert Into Customers Values
+   ('Rogina', 'Irshaidat', 'Full-Stack'),
+   ('Nouran', 'Omar', 'Full-Stack'),
+   ('Razan', 'Smady', 'Full-Stack');
+Select * From Customers;
+
+Insert Into Orders Values
+   (1, 258, 3),
+   (2, 896, 4),
+   (3, 748, 5);
+Select * From Orders;
+
+Insert Into Product Values
+   (1,'Qais', 586),
+   (2,'Yazeed', 125),
+   (3,'Mayyas', 695);
+Select * From Product;
+
+Select * from Customers
+Join Orders On Customers.CustomerID = Orders.CustomerID;
+
+Select * from Customers
+Left Join Orders On Customers.CustomerID = Orders.CustomerID;
+
+Select * from Orders
+Join Product On Orders.OrderID = Product.OrderID;
+
+Select * from Orders
+Right Join Product On Orders.OrderID = Product.OrderID;
+
+
+
+
+
+
+
+
